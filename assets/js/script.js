6d103698d363a5a1eb125d21d8704f4bc80824b7
404pandas <113853550+404pandas@users.noreply.github.com>
2023-04-01 19:05:43 -0400

    Boiler code, palette set,ready for dependencies

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// Global variables
+
+// DOM element references
+
+// Add timezone plugins to day.js
+
+// Function to display the search history list.
+
+// Start at end of history array and count down to show the most recent at the top.
+// `data-search` allows access to city name when click handler is invoked
+
+// Function to update history in local storage then updates displayed history.
+// If there is no search term return the function
+
+// Function to get search history from local storage
+
+// Function to display the current weather data fetched from OpenWeather api.
+// Store response data from our fetch request in variables
+
+// Function to display a forecast card given an object from open weather api daily forecast.
+// variables for data from api
+// Create elements for a card
+// Add content to elements
+
+// Function to display 5 day forecast.
+// Create unix timestamps for start and end of 5 day forecast
+
+// First filters through all of the data and returns only data that falls between one day after the current data and up to 5 days later.
+// Then filters through the data and returns only data captured at noon for each day.
+
+// Fetches weather data for given location from the Weather Geolocation
+// endpoint; then, calls functions to display current and forecast weather data.
+
+// Fetches coord data for given location from the Weather Geolocation endpoint
+
+// Function to handle search form submit
+// Don't continue if there is nothing in the search form
+
+// Function to handle search form click
+// Don't do search if current elements is not a search history button
+
+// Init call
+
+// Event listeners (Form and history)
