344d8e5328aecf8da4e69ade9fa4588e840e40b0
404pandas <113853550+404pandas@users.noreply.github.com>
2023-04-02 20:08:03 -0400

    Built out search and append functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,13 @@ var weatherAPIurl = "https://api.openweathermap.org";
 var weatherAPIkey = "b917dd3be7f85cdcbd69de2a255eb995";
 
 // DOM element references
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 var searchForm = document.querySelector("#searchInput");
 var searchInput = document.querySelector("#searchForm");
 var currentContainer = document.querySelector("#current");
 var forecastContainer = document.querySelector("#forecast");
 var historyContainer = document.querySelector("#history");
+var errorAlert = document.querySelector("#error");
 // Opt + Click = add cursor line
 
 // Add timezone plugins to day.js
@@ -35,11 +37,13 @@ function renderCurrent(city, weather) {
   // day.js stuff
   var date = dayjs().format("MM/DD/YYYY");
   // Store response data from our fetch request in variables
-  var temp = weather.main.temp;
-  var wind = weather.wind.speed;
+  var tempF = weather.main.temp;
+  var windMph = weather.wind.speed;
   var humidity = weather.main.humidity;
   // icon- future development
 
+  //   Create element variables
+  // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
   var cardContainer = document.createElement(" div");
   var cardImgDiv = document.createElement("div");
   var cardImg = document.createElement("img");
@@ -47,48 +51,85 @@ function renderCurrent(city, weather) {
   var cardContent = document.createElement("div");
   var cardTitle = document.createElement("span");
   var moreIcon = document.createElement("i");
+  //   Future development- dynamic link
   var dynamicLink = document.createElement("a");
   var cardReveal = document.createElement("div");
   var cardTitleSecond = document.createElement("span");
   var closeIcon = document.createElement("i");
   var ul = document.createElement("ul");
-  var date = document.createElement("h2");
-  var temp = document.createElement("h2");
-  var wind = document.createElement("h2");
-  var humidity = document.createElement("h2");
+  var li = document.createElement("li");
+  var dateCreate = document.createElement("h2");
+  var tempCreate = document.createElement("h2");
+  var windCreate = document.createElement("h2");
+  var humidityCreate = document.createElement("h2");
 
-  //   var cardContainer = document.createElement(' div'); div w/ class card parent
-  //   var cardImgDiv = document.createElement('div');  div w/ class card-image waves-effect waves-block waves-light child
-  //   var cardImg = document.createElement('img');  img w/ class activator src ./assets/images/weather-specific/pooh96.png grandchild
+  // creates a div and sets class
+  cardContainer.setAttribute("class", "card");
+
+  //   Creates a div and sets class
+  cardImgDiv.setAttribute(
+    "class",
+    "card-image waves-effect waves-block waves-light"
+  );
+  //   Appends cardImgDiv to cardContainer
+  cardContainer.append(cardImgDiv);
+  //   Creates img and sets class, src, and id
+  cardImg.setAttribute("class", "activator");
+  cardImg.setAttribute("src", "./assets/images/weather-specific/pooh96.png");
+  cardImg.setAttribute("id", "randomImage");
+  //   math.random method to randomly generate img src
+  document.querySelector(
+    "#randomImage"
+  ).src = `./assets/images/weather-specific/pooh${Math.floor(
+    Math.random() * 7
+  )}.png`;
+
+  cardContainer.append(cardImg);
+
+  cardContent.setAttribute("class", "card-content");
+  cardContainer.append(cardContainer);
+
+  cardTitle.setAttribute(
+    "class",
+    "card-title activator grey-text text-darken-4 flexbox flexNoWrap"
+  );
+  cardTitle.setAttribute("id", "cardTitleId");
+  //   https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+  document.getElementById("cardTitleId").textContent = `${city} + ${date}`;
+
+  moreIcon.setAttribute("class", "material-icons right");
+  cardTitle.append(moreIcon);
   // Future development- dynamic src for weather
-  //   var cardContent = document.createElement('div');  div w/ class card-content child
-  //   var cardTitle = document.createElement('span');  span w/ class grandchildcard-title activator grey-text text-darken-4 flexbox flexNoWrap
-  //   var moreIcon = document.createElement('i');  i w/ class material-icons right great-grandchild
-  // var dynamicLink = document.createElement('a');  a w/ href dynamicLink onclick RedirectURL();return false;
+
+  // Future Development- var dynamicLink = document.createElement('a');  a w/ href dynamicLink onclick RedirectURL();return false;
+
   //   var cardReveal = document.createElement('div');  div class card-reveal child
-  //   var cardTitleSecond = document.createElement('span');  span class card-title grey-text text-darken-4 grandchild
-  //   var closeIcon = document.createElement('i');  i class material-icons right great grandchild
-  //   var ul = document.createElement('ul'); ul no classes
-  // var date = document.createElement('h2');  h2
-  // var temp = document.createElement('h2');  h2
-  //   var wind = document.createElement('h2');  h2
-  //   var humidity = document.createElement('h2');  h2
+  cardReveal.setAttribute("class", "card-reveal");
+  cardContainer.append(cardReveal);
 
-  // SetAttribute and class card
-  //   row > col > card > card-content > card-title span > p > future development = card-action
+  cardTitleSecond.setAttribute("class", "card-title grey-text text-darken-4");
+  cardReveal.append(cardTitleSecond);
+  closeIcon.setAttribute("class", "material-icons right");
+  closeIcon.textContent = "close";
+  cardTitleSecond.append(closeIcon);
 
-  // SetAttribute and card body
-  // append cardBody
-
-  // textContent card-title
-  // set attribute icons
-  // append icon
+  cardReveal.append(ul);
+  li.appendChild(dateCreate).setAttribute("id", "date");
+  li.appendChild(tempCreate).setAttribute("id", "temp");
+  li.appendChild(windCreate).setAttribute("id", "wind");
+  li.appendChild(humidityCreate).setAttribute("id", "humidity");
 
   // text content for temp, wind, humidity
+  cardTitle.textContent = dayjs(forecast.dt_txt).format("MM/DD/YYYY");
+  document.querySelector("#temp").textContent = `Temp: ${tempF}`;
+  document.querySelector("#wind").textContent = `Wind: ${windMph}`;
+  document.querySelector("#humidity").textContent = `Humidity: ${humidity}`;
   // append heading, temp, wind, humidity
 
   // innerHTML to clear w/ empty string
+  currentContainer.innerHTML = "";
   // append entire card
+  currentContainer.append(cardContainer);
 }
 
 // Function to display a forecast card given an object from open weather api daily forecast.
@@ -112,7 +153,7 @@ function renderItems(city, data) {
   // call renderCurrent
   renderCurrent(city, data.list[0], data.city.timezone);
   // call renderForecast
-  renderForecast(data.list);
+  //   renderForecast(data.list);
 }
 
 // Function to fetch weather data for given location from the Weather Geolocation
@@ -133,6 +174,7 @@ function fetchWeather(location) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data);
       renderItems(city, data);
     })
     .catch(function (err) {
@@ -154,10 +196,10 @@ function fetchCoords() {
     .then(function (data) {
       // Not found
       if (!data[0]) {
-        notFound();
+        errorAlert.removeClass("hidden");
       } else {
         // call appendSearchHistory(search)+fetchWeather
-        appendSearchHistory(search);
+        // appendSearchHistory(search);
         fetchWeather(data[0]);
       }
       // Else
@@ -168,21 +210,35 @@ function fetchCoords() {
 }
 
 // Function to handle search form submit
-function handleSearchFormSubmit() {
+function handleSearchFormSubmit(event) {
   // Don't continue if there is nothing in the search form
+  //   if logic with !searchInput return
+  if (!searchInput.value) {
+    // removeAttribute removes hidden attribute from alert
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttribute
+    errorAlert.removeClass("hidden");
+    return;
+  }
+  //   preventDefault tells agent that if event does not
+  // get explicitly handled, default action should not be taken
+  event.preventDefault();
   //   Call fetchCoords(search)
+  //   Creates variable and fills it with the value from search input
+  // Trim removes whitespace from both ends of a string
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+  // Value returns an array of object's property values
+  var search = searchInput.value.trim();
+  //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
+  //   check fetchCoords with weather api
+  fetchCoords(search);
+  //   Turns value into a string
+  searchInput.value = "";
 }
 
 // Function to handle search form click
-function handleSearchFormClick() {
+function handleSearchHistoryClick() {
   // Don't do search if current elements is not a search history button
   // Call fetchCoords(search)
-}
-
-// NotFound alert generation
-function notFound() {
-  // remove .hidden
-  // setTimeout
 }
 
 // Dynamic URL for card link
@@ -229,5 +285,7 @@ function RedirectURL() {
 getSearchHistory();
 
 // Event listeners (Form and history)
-searchForm;
-searchHistory;
+// Search button
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+// history button
+searchHistory.addEventListener("click", handleSearchHistoryClick);
